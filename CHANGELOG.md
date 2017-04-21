1ddf31d27b59f0aaebd1ea36416b9aa2b317380e
zhhyang <zhhyang0404@163.com>
2017-04-21 18:06:56 +0800

    TabPane新增innerHtml属性，支持dangerouslySetInnerHTML

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# 更新日志
+
+## 1.2.0    - 2017-04-21
+
+### 修复
+* TabPane新增innerHtml属性，支持dangerouslySetInnerHTML
+
+---
--- src/TabPane.js
+++ src/TabPane.js
@@ -1,21 +1,32 @@
 import React, { Component }from 'react'
 import PropTypes from 'prop-types'
-import classnames from 'classnames'
+import classNames from 'classnames'
 
 class TabPane extends Component {
 
   render () {
     const props = this.props
-    const {className, destroyInactiveTabPane, active, forceRender} = props
+    const {className, destroyInactiveTabPane, active, forceRender,placeholder,innerHtml,children} = props
     this._isActived = this._isActived || active
     const prefixCls = `${props.rootPrefixCls}-tabpane`
-    const cls = classnames({
+    const cls = classNames({
       [prefixCls]: 1,
       [`${prefixCls}-inactive`]: !active,
       [`${prefixCls}-active`]: active,
       [className]: className,
     })
     const isRender = destroyInactiveTabPane ? active : this._isActived
+    if (innerHtml){
+      return (
+        <div
+          style={props.style}
+          role="tabpanel"
+          aria-hidden={props.active ? 'false' : 'true'}
+          className={cls}
+          dangerouslySetInnerHTML={{__html: isRender || forceRender ? children : placeholder}}
+        />
+      )
+    }
     return (
       <div
         style={props.style}
@@ -23,7 +34,7 @@ class TabPane extends Component {
         aria-hidden={props.active ? 'false' : 'true'}
         className={cls}
       >
-        {isRender || forceRender ? props.children : props.placeholder}
+        {isRender || forceRender ? children : placeholder}
       </div>
     )
   }
@@ -35,9 +46,11 @@ TabPane.propTypes = {
   destroyInactiveTabPane: PropTypes.bool,
   forceRender: PropTypes.bool,
   placeholder: PropTypes.node,
+  innerHtml: PropTypes.bool,
 }
 
 TabPane.defaultProps = {
-  placeholder: null
+  placeholder: null,
+  innerHtml:false
 }
 export default TabPane
